94b19cf1bc5d0e348c802c147077d89afa87b32f
elwintay <fd.techies@gmail.com>
2024-08-31 23:46:53 +0800

    FEAT changing login and logout for navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,48 +37,38 @@ const NavBar = (props) => {
     const loggedOut = <div className="md:flex lg:flex-1 items-center justify-end font-normal hidden">
                         <ul className="flex gap-8 mr-16 text-[18px]">
                             <Link spy={true} smooth={true} to="/login">
-                                <li className='px-5 py-1 hover:text-blue-400 transition border-b-2 border-slate-900 cursor-pointer'>Login</li>
-                            </Link>
-                            <Link spy={true} smooth={true} onClick={clearCookie}>
-                                <li className='px-5 py-1 hover:text-blue-400 transition border-b-2 border-slate-900 cursor-pointer'>Logout</li>
-                            </Link>
-                            <Link spy={true} smooth={true} to="/signup">
-                                <li className='bg-slate-500 px-5 py-1 rounded-full hover:text-blue-400 transition border-b-2 border-slate-900 cursor-pointer'>Sign Up</li>
+                                <li className='bg-slate-500 px-5 py-1 rounded-full hover:text-blue-400 transition border-b-2 border-slate-900 cursor-pointer'>Login</li>
                             </Link>
                         </ul>
                     </div>
-    const loggedOutFlex = <div>
-                            <div className='lg:hidden block absolute top-12 w-full left-0 right-0 bg-slate-900 transition'>
-                                <ul className="text-center text-xl p-10">
-                                    <Link spy={true} smooth={true} to="/login">
-                                        <li className='my-2 py-2 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Login</li>
-                                    </Link>
-                                    <Link spy={true} smooth={true} onClick={clearCookie}>
-                                        <li className='my-2 py-2 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Logout</li>
-                                    </Link>
-                                    <Link spy={true} smooth={true} to="/signup">
-                                        <li className='bg-slate-500 my-2 py-2 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Sign up</li>
-                                    </Link>
-                                </ul>
-                            </div>
-                        </div>
-    const loggedInFlex = <div>
-                            <div className='lg:hidden block absolute top-12 w-full left-0 right-0 bg-slate-900 transition'>
-                                <ul className="text-center text-xl p-10">
-                                    <Link spy={true} smooth={true} onClick={clearCookie}>
-                                        <li className='my-2 py-2 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Logout</li>
-                                    </Link>
-                                </ul>
-                            </div>
-                        </div>
     const loggedIn = <div className="md:flex lg:flex-1 items-center justify-end font-normal hidden">
-                        <span className='text-m p-4'>Welcome {user}</span>
+                        <span className='text-m p-4 text-blue-400'>Welcome {user}</span>
                         <ul className="flex gap-8 mr-16 text-[18px]">
                             <Link spy={true} smooth={true} onClick={clearCookie}>
                                 <li className='bg-slate-500 px-5 py-1 rounded-full hover:text-blue-400 transition border-b-2 border-slate-900 cursor-pointer'>Logout</li>
                             </Link>
                         </ul>
                     </div>
+    const loggedOutFlex = <div>
+                            <div className='lg:hidden block absolute top-12 w-full left-0 right-0 bg-slate-900 transition'>
+                                
+                                <ul className="text-center text-xl p-10">
+                                    <Link spy={true} smooth={true} to="/login">
+                                        <li className='bg-slate-500 my-2 py-2 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Login</li>
+                                    </Link>
+                                </ul>
+                            </div>
+                        </div>
+   const loggedInFlex = <div>
+                            <div className='lg:hidden block absolute top-12 w-full left-0 right-0 bg-slate-900 transition'>
+                                <ul className="text-center text-xl p-10">
+                                    <li><span className='text-m p-4 text-blue-400'>Welcome {user}</span></li>
+                                    <Link spy={true} smooth={true} onClick={clearCookie}>
+                                        <li className='bg-slate-500 my-2 py-2 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Logout</li>
+                                    </Link>
+                                </ul>
+                            </div>
+                        </div>
     
     return (
         <nav className='bg-slate-900'>
@@ -88,7 +78,7 @@ const NavBar = (props) => {
                 </div>
                 {isLoggedIn? loggedIn : loggedOut}
                 <div>
-                    {nav && loggedOutFlex}
+                    {(nav && isLoggedIn) ? loggedInFlex : (nav && !isLoggedIn) && loggedOutFlex}
                 </div>
                 <button className="block md:hidden transition" onClick={handleNav}>
                     {nav ? <FaTimes/> : <CiMenuFries/>}
